5749f0f7c4dcc36a0130f88c9220daf8facc1deb
Niki-Mozzon <83493704+Niki-Mozzon@users.noreply.github.com>
2021-05-19 12:39:50 +0200

    Character and Orders

---
+++ wa_Legend/wa_Legend/Legend_1/Migrations/20210518160401_AddCharacter2Db2.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Legend_1.Migrations
+{
+    public partial class AddCharacter2Db2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Orders_CharacterId",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "CharacterId",
+                table: "Characters",
+                newName: "OrderId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_CharacterId",
+                table: "Characters",
+                newName: "IX_Characters_OrderId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Orders_OrderId",
+                table: "Characters",
+                column: "OrderId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Characters_Orders_OrderId",
+                table: "Characters");
+
+            migrationBuilder.RenameColumn(
+                name: "OrderId",
+                table: "Characters",
+                newName: "CharacterId");
+
+            migrationBuilder.RenameIndex(
+                name: "IX_Characters_OrderId",
+                table: "Characters",
+                newName: "IX_Characters_CharacterId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Characters_Orders_CharacterId",
+                table: "Characters",
+                column: "CharacterId",
+                principalTable: "Orders",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
---
+++ wa_Legend/wa_Legend/Legend_1/Models/Order.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Legend_1.Models
+{
+    public class Order
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        [DisplayName("Order")]
+        public string NameOrder { get; set; }
+        
+    }
+}
